0e7001776179628198c8a81423ab5c7c57753f54
Wai Lee Boo <waileeboo@gmail.com>
2025-11-16 00:05:44 +0000

    add patch file:

--- fixed/run.sh
+++ fixed/run.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 docker build -t sshs_asg2 -f Dockerfile .
-docker run -v .:/workspace -it sshs_asg2
+docker run -v .:/workspace -it sshs_asg2 bash -c "
+    echo '-----------------------------------------------------' ;
+    echo 'testing original binary with crash input before applying patch' ;
+    ./js_original crash_000000 || echo 'CRASH';
+    echo '------------------------------------------------------' ;
+    echo 'testing fixed binary with crash input after applying patch';
+    ./js_fixed crash_000000;
+    echo '------------------------------------------------------' ;"
